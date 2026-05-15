05bd03be8aedca4e71dfce8ae0bec2491e06f77b
Chae-Yu <chaeyu2004@naver.com>
2026-05-15 12:05:06 +0900

    Setting: Spring Boot 초기 세팅 설정

---
+++ src/main/java/com/woorifisa/won_invest_channel_server/WonInvestChannelServerApplication.java
@@ -0,0 +1,13 @@
+package com.woorifisa.won_invest_channel_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WonInvestChannelServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WonInvestChannelServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/woorifisa/won_invest_channel_server/WonInvestChannelServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.woorifisa.won_invest_channel_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WonInvestChannelServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
