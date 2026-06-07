384b9b748c29615bc4a943bc35b81a4996c9c783
chaeyu <chaeyu2004@naver.com>
2026-06-07 19:55:35 +0900

    Chore: 스웨거 헤더 추가

--- src/main/java/com/woorifisa/won_invest_channel_server/global/config/SwaggerConfig.java
+++ src/main/java/com/woorifisa/won_invest_channel_server/global/config/SwaggerConfig.java
@@ -11,22 +11,36 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class SwaggerConfig {
 
+    private static final String JWT_SECURITY_SCHEME = "JWT";
+    private static final String SERVICE_ID_SECURITY_SCHEME = "SERVICE_ID";
+    private static final String INTERNAL_API_KEY_SECURITY_SCHEME = "INTERNAL_API_KEY";
+
     @Bean
     public OpenAPI openAPI() {
-        SecurityScheme securityScheme = new SecurityScheme()
-                .type(SecurityScheme.Type.HTTP)
-                .scheme("bearer")
-                .bearerFormat("JWT")
-                .in(SecurityScheme.In.HEADER)
-                .name("Authorization");
+        Components components = new Components()
+                .addSecuritySchemes(JWT_SECURITY_SCHEME, new SecurityScheme()
+                        .type(SecurityScheme.Type.HTTP)
+                        .scheme("bearer")
+                        .bearerFormat("JWT")
+                        .in(SecurityScheme.In.HEADER)
+                        .name("Authorization"))
+                .addSecuritySchemes(SERVICE_ID_SECURITY_SCHEME, new SecurityScheme()
+                        .type(SecurityScheme.Type.APIKEY)
+                        .in(SecurityScheme.In.HEADER)
+                        .name("X-Service-ID"))
+                .addSecuritySchemes(INTERNAL_API_KEY_SECURITY_SCHEME, new SecurityScheme()
+                        .type(SecurityScheme.Type.APIKEY)
+                        .in(SecurityScheme.In.HEADER)
+                        .name("X-Internal-Api-Key"));
 
-        SecurityRequirement securityRequirement = new SecurityRequirement().addList("bearerAuth");
+        SecurityRequirement securityRequirement = new SecurityRequirement()
+                .addList(JWT_SECURITY_SCHEME);
 
         return new OpenAPI()
                 .info(new Info()
                         .title("INVEST-CHANNEL API 명세서")
                         .version("v1.0"))
                 .addSecurityItem(securityRequirement)
-                .components(new Components().addSecuritySchemes("bearerAuth", securityScheme));
+                .components(components);
     }
 }
