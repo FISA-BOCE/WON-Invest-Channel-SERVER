a67fb78552a0f96fa94d2e2b3306ac2fc217dc57
chaeyu <chaeyu2004@naver.com>
2026-06-07 20:26:20 +0900

    Rename: 카드 채널과 FeignConfig 네이밍 통일

--- src/main/java/com/woorifisa/won_invest_channel_server/domain/account/external/InvestCoreAccountApi.java
+++ src/main/java/com/woorifisa/won_invest_channel_server/domain/account/external/InvestCoreAccountApi.java
@@ -2,7 +2,7 @@ package com.woorifisa.won_invest_channel_server.domain.account.external;
 
 import com.woorifisa.won_invest_channel_server.domain.account.dto.request.CreateInvestAccountRequest;
 import com.woorifisa.won_invest_channel_server.domain.account.external.dto.InvestAccountCoreResponse;
-import com.woorifisa.won_invest_channel_server.global.config.FeignConfig;
+import com.woorifisa.won_invest_channel_server.global.config.InternalFeignConfig;
 import com.woorifisa.won_invest_channel_server.global.response.ApiResponse;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RequestHeader;
 
 import java.util.UUID;
 
-@FeignClient(name = "invest-core", url = "${internal.services.invest-core.base-url}", configuration = FeignConfig.class)
+@FeignClient(name = "invest-core", url = "${internal.services.invest-core.base-url}", configuration = InternalFeignConfig.class)
 public interface InvestCoreAccountApi {
 
     @PostMapping("/internal/invest/accounts/new")
